d14b89da908fb3f48f82c8cf9dcc5be6b23a8f64
notS0LD13R <joelsebin@gmail.com>
2023-09-10 18:45:19 +0530

    feat(gallery):finished

--- src/App.tsx
+++ src/App.tsx
@@ -4,27 +4,27 @@ import Navbar from "./Components/Navbar/Navbar";
 import Home from "./Components/Home/Home";
 import About from "./Components/About/About";
 import Events from "./Components/Events/Events";
-import Memories from "./Components/Memories/Memories";
+import Gallery from "./Components/Gallery/Gallery";
 import Statistics from "./Components/Statistics/Statistics";
 import ExploreLC from "./Components/ExploreLC/ExploreLC";
 import Team from "./Components/Team/Team";
 import Connect from "./Components/Connect/Connect";
 import Footer from "./Components/Footer/Footer";
 function App() {
-  return (
-    <div className="appWrapper">
-      <Navbar />
-      <Home />
-      <About />
-      <Events />
-      <Memories />
-      <Statistics />
-      <ExploreLC />
-      <Team />
-      <Connect />
-      <Footer />
-    </div>
-  );
+    return (
+        <div className="appWrapper">
+            <Navbar />
+            <Home />
+            <About />
+            <Events />
+            <Gallery />
+            <Statistics />
+            <ExploreLC />
+            <Team />
+            <Connect />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
--- src/Components/Memories/Memories.tsx
+++
@@ -1,7 +0,0 @@
-import styles from "./Memories.module.css";
-
-const Memories = () => {
-  return (<>Memories</>);
-};
-
-export default Memories;
