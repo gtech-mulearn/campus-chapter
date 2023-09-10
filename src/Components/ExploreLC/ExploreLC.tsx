20e0770d22e2c2947d51b95fe79761df188a3607
Amal C P <acp070146@gmail.com>
2023-09-10 17:36:50 +0530

    Explore learning completed

--- src/Components/ExploreLC/ExploreLC.tsx
+++ src/Components/ExploreLC/ExploreLC.tsx
@@ -1,7 +1,24 @@
 import styles from "./ExploreLC.module.css";
+import image from "./assets/explorelc.svg";
 
 const ExploreLC = () => {
-  return <>ExploreLC</>;
+  return (
+    <div className={styles.exploreLCWrapper}>
+      <div className={styles.imageWrapper}>
+        <img src={image} alt="" />
+      </div>
+      <div className={styles.contentWrapper}>
+        <h1>Explore Learning Circles</h1>
+        <p>
+          An informal mechanism for bringing together learners who are
+          interested in the same topic from across different fields and
+          disciplines. A fantastic way to spend a small amount of time learning
+          about new things with a group of people with same interests!
+        </p>
+        <a href="">Create/Join Learning Circles</a>
+      </div>
+    </div>
+  );
 };
 
 export default ExploreLC;
