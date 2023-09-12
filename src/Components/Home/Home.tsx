b44f4cfa3ecde8ec5ab69530976bf23ce8666a12
Amal C P <acp070146@gmail.com>
2023-09-12 10:18:55 +0530

    home page update

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,6 +1,6 @@
 import styles from "./Home.module.css";
 import image from "./assets/homeimg.svg";
-import { BlueStar,WhiteStar } from "./assets/svg";
+import { BlueStar, WhiteStar,Mu } from "./assets/svg";
 
 const Home = () => {
   return (
@@ -11,8 +11,21 @@ const Home = () => {
         <p>at Christ College of Engineering!</p>
       </div>
       <img src={image} alt="" />
-
-      {/* <BlueStar size="20px" /> */}
+      <div className={styles.start1}>
+        <BlueStar size="50px" />
+      </div>{" "}
+      <div className={styles.start2}>
+        <BlueStar size="70px" />
+      </div>{" "}
+      <div className={styles.start3}>
+        <WhiteStar size="40px" />
+      </div>
+      <div className={styles.start4}>
+        <WhiteStar size="40px" />
+      </div>{" "}
+      <div className={styles.start4}>
+        <Mu size="40px" />
+      </div>
     </div>
   );
 };
