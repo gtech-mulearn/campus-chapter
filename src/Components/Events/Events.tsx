7b552e89af2c67a9c5edb41e19eda2650b8f48ad
Jenin82 <jenin8282@gmail.com>
2023-09-13 07:03:10 +0530

    fix(): removed unwanted variables and imports

--- src/Components/Events/Events.tsx
+++ src/Components/Events/Events.tsx
@@ -1,39 +1,7 @@
 import styles from "./Events.module.css";
-import img from "./assets/a4b43559a5f3a8b9a7986a561e1a9619.png";
 import data from "../../../data.json";
 
 const Events = () => {
-    const mainCard = {
-        month: "Sep",
-        date: "29",
-        head: "Opportunities in TCS EIS",
-        para: "Webinar on Engineering and Industrial Services division of TCS.",
-        img: img,
-    };
-    const cardData = [
-        {
-            month: "Sep",
-            date: "29",
-            head: "Opportunities in TCS EIS",
-            para: "Webinar on Engineering and Industrial Services division of TCS.",
-            img: img,
-        },
-        {
-            month: "Sep",
-            date: "29",
-            head: "Opportunities in TCS EIS",
-            para: "Webinar on Engineering and Industrial Services division of TCS.",
-            img: img,
-        },
-        {
-            month: "Sep",
-            date: "29",
-            head: "Opportunities in TCS EIS",
-            para: "Webinar on Engineering and Industrial Services division of TCS.",
-            img: img,
-        },
-    ];
-
     return (
         <div className={styles.events}>
             <h2>Our Event Journey</h2>
